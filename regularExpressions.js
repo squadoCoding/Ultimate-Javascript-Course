059eab4f14501ff51e73ddebce2407cbbb0af120
squadoCoding <106444206+squadoCoding@users.noreply.github.com>
2022-11-09 17:44:30 +0530

    Regular expressions in javascript

---
+++ regularExpressions.js
@@ -0,0 +1,5 @@
+// Regualr expressions in javascript
+const regexp = /[A-Z]\w+/g;
+const str = "Siddhant is a very very good and very nice very very coder";
+
+console.log(str.replace(regexp, "VERY"));
